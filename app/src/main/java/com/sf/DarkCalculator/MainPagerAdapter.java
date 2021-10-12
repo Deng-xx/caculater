5d435ba974485544dc51e66195d23d178f4afce4
Flandre16Scarlet <1377891791@qq.com>
2021-10-12 12:19:07 +0800

    commit1

---
+++ app/src/main/java/com/sf/DarkCalculator/MainPagerAdapter.java
@@ -0,0 +1,40 @@
+package com.sf.DarkCalculator;
+
+import androidx.viewpager.widget.PagerAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+
+
+public class MainPagerAdapter extends PagerAdapter {
+    private List<View> pageList;
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    public MainPagerAdapter(List<View> pageList) {
+        super();
+        this.pageList = pageList;
+    }
+
+    @Override
+    public int getCount() {
+        return pageList.size();
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, int position) {
+        View view = pageList.get(position);
+        container.addView(view);
+        return view;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView(pageList.get(position));
+    }
+}
